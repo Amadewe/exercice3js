d82a78d71cab1d0fa9947ba219fab914669109c9
Amadewe <dewevre.amandine@gmail.com>
2019-01-22 10:19:22 +0100

    Correction de l'exercice 3

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,3 @@
-var userName = prompt('Entrez votre prénom :');
-alert('Bonjour : ' + userName); // Affiche le prénom entré par l'utilisateur
+var firstName = prompt('Entrez votre prénom :');
+alert('Bonjour : ' + firstName); // Affiche le prénom entré par l'utilisateur
+// fonction : prompt entre (paramètre); si on met var devant la fonction on récupère le retour de la fonction
